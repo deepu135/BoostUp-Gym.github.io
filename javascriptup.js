eb22ae4dab5b2837516880d8a9e06fcb87e54b6a
deepu135 <nitishpardhan67@gmail.com>
2025-08-12 10:47:39 +0530

    Update javascriptup.js

--- javascriptup.js
+++ javascriptup.js
@@ -1,3 +1,20 @@
+//navigation
+let icon = Document.querySelector(".icon");
+let ul = document.querySelector("ul");
+
+icon.addEventListener("click", ()=>{
+  ul.classList.toggle("showData");
+  console.log(ul);
+  if(ul.className == "showData")
+  {
+    document.getElementById("bar").className="fa-solid fa-xmark";
+  }
+  else{
+    document.getElementById("bar").className="ri-menu-line";
+  }
+})
+
+
 // Contact Form Validation
 document.getElementById("contactForm").addEventListener("submit", function(e) {
   e.preventDefault();
